23df06c3d5451f4e1bba61b2a92cffe4d1b47ff1
赵元达 <yuandabest@163.com>
2023-12-25 15:29:50 +0800

    feat

--- bin/cli.js
+++ bin/cli.js
@@ -1,55 +1,52 @@
 #! /usr/bin/env node
 
-const program = require('commander');
-const chalk = require('chalk');
-const figlet = require('figlet');
+const program = require("commander");
+const chalk = require("chalk");
+const figlet = require("figlet");
 
 program
   // 定义命令和参数
-  .command('create <app-name>')
-  .description('create a new project')
+  .command("create <app-name>")
+  .description("create a new project")
   // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
-  .option('-f, --force', 'overwrite target directory if it exist')
+  .option("-f, --force", "overwrite target directory if it exist")
   .action((name, options) => {
     // 打印执行结果
     // console.log('name:',name,'options:',options);
-    require('../lib/create')(name, options);
-  })
+    require("../lib/create")(name, options);
+  });
 
 // 配置 config 命令
 program
-  .command('config [value]')
-  .description('inspect and modify the config')
-  .option('-g, --get <path>', 'get value from option')
-  .option('-s, --set <path> <value>')
-  .option('-d, --delete <path>', 'delete option from config')
+  .command("config [value]")
+  .description("inspect and modify the config")
+  .option("-g, --get <path>", "get value from option")
+  .option("-s, --set <path> <value>")
+  .option("-d, --delete <path>", "delete option from config")
   .action((value, options) => {
-    console.log(value, options)
-  })
+    console.log(value, options);
+  });
 
-// program
-// .command('oi [value]')
-//   .option('-d, --debug', 'output extra debugging')
-//   .option('-s, --small', 'small pizza size')
-
-program
-  .on('--help', () => {
-    // 使用 figlet 绘制 Logo
-    console.log('\r\n' + figlet.textSync('future', {
-      font: 'Ghost',
-      horizontalLayout: 'full',
-      verticalLayout: 'full',
-      width: 80,
-      whitespaceBreak: true
-    }));
-    // 新增说明信息
-    console.log(`\r\nRun ${chalk.cyan(`future <command> --help`)} show details\r\n`)
-  })
+program.on("--help", () => {
+  // 使用 figlet 绘制 Logo
+  console.log(
+    "\r\n" +
+      figlet.textSync("xc", {
+        font: "Ghost",
+        horizontalLayout: "full",
+        verticalLayout: "full",
+        width: 80,
+        whitespaceBreak: true,
+      })
+  );
+  // 新增说明信息
+  console.log(`\r\nRun ${chalk.cyan(`xc <command> --help`)} show details\r\n`);
+});
 
 program
   // 配置版本号信息
-  .version(`v${require('../package.json').version}`)
-  .usage('<command> [option]')
+  .version(`v${require("../package.json").version}`)
+  .usage("<command> [option]");
 
 // 解析用户执行命令传入参数
 program.parse(process.argv);
